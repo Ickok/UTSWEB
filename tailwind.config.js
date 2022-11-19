def18dc1d0cbfbab3d095f0117aff27fb7325809
Ikock <106673912+Ickok@users.noreply.github.com>
2022-11-19 12:42:10 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#0EC5D7',
+                secondary: '#ff3b07',
+                nav: '#969696',
+                header: '#F6F9FC',
+                buttom: '#002125',
+                transparant: '#000000 15%'
+            },
+            fontFamily: {
+                raleway: ['Raleway', 'sans-serif'],
+                nunito: ['Nunito Sans', 'sans-serif'],
+                poppins: ['Poppins','sans-serif']
+            }
+        },
+    },
+}
